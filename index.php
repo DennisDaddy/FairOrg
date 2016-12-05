9ba11eed4383ab8a8e499dde784d5934f4cb2562
DennisDaddy <softteez95@gmail.com>
2016-12-05 13:59:53 +0300

    add carousel stylings

--- index.php
+++ index.php
@@ -63,7 +63,7 @@
                 <div class="item active" style="background-image: url(images/slider/bg1.jpg)">
                     <div class="container">
                         <div class="row">
-                            <div class="col-sm-12">
+                            <div class="col-sm-6">
                                 <div class="carousel-content centered">
                                     <h2 class="animation animated-item-1">Powerful and Responsive Web Design</h2>
                                     <p class="animation animated-item-2">Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
@@ -75,12 +75,12 @@
                 <div class="item" style="background-image: url(images/slider/bg2.jpg)">
                     <div class="container">
                         <div class="row">
-                            <div class="col-sm-12">
+                            <div class="col-sm-6">
                                 <div class="carousel-content center centered">
                                     <h2 class="boxed animation animated-item-1">Clean, Crisp, Powerful and Responsive Web Design</h2>
                                     <p class="boxed animation animated-item-2">Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
                                     <br>
-                                    <a class="btn btn-md animation animated-item-3" href="#">Learn More</a>
+                                    <a class="btn btn-md  boxed animation animated-item-2" href="#">Learn More</a>
                                 </div>
                             </div>
                         </div>
