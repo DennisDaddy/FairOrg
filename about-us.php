f91e8907873a24b9eed1b4abe36d4a146270ca92
DennisDaddy <softteez95@gmail.com>
2016-12-05 12:30:18 +0300

    change to php

--- about-us.php
+++ about-us.php
@@ -62,7 +62,7 @@
                 </div>
                 <div class="col-sm-6">
                     <ul class="breadcrumb pull-right">
-                        <li><a href="index.html">Home</a></li>
+                        <li><a href="index.php">Home</a></li>
                         <li class="active">About Us</li>
                     </ul>
                 </div>
